68017757ae3c3b3becc1f8f71351bf367d6b31d9
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2024-08-22 23:02:02 +0800

    Modify .gitignore and build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    id("java-library")
+    id("maven-publish")
 }
 
 apply {
@@ -8,7 +9,7 @@ apply {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(11));
+    toolchain.languageVersion = JavaLanguageVersion.of(11)
 }
 
 repositories {
@@ -20,10 +21,65 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+    test {
+        useJUnitPlatform()
+    }
+    processResources.get().apply {
+        from(file("LICENSE")) { into("META-INF/") }
+    }
+    jar.get().manifest.attributes(
+        mapOf(
+            "Specification-Title" to "content-disposition",
+            "Specification-Version" to "${project.version}",
+            "Specification-Vendor" to "teacon.org",
+            "Implementation-Title" to "content-disposition",
+            "Implementation-Version" to "${project.version}",
+            "Implementation-Vendor" to "teacon.org",
+        )
+    )
 }
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    publications.create<MavenPublication>("mavenJava") {
+        groupId = "org.teacon"
+        version = "${project.version}"
+        artifactId = "content-disposition"
+        pom {
+            name = ("ContentDisposition")
+            url = "https://github.com/teaconmc/content-disposition"
+            description = "Java implementation of parsing and constructing content-disposition (RFC6266) header values"
+            licenses {
+                license {
+                    name = "GNU Lesser General Public License, Version 3.0"
+                    url = "https://www.gnu.org/licenses/lgpl-3.0.txt"
+                }
+            }
+            developers {
+                developer {
+                    id = "teaconmc"
+                    name = "TeaConMC"
+                    email = "contact@teacon.org"
+                }
+                developer {
+                    id = "ustc-zzzz"
+                    name = "Yanbing Zhao"
+                    email = "zzzz.mail.ustc@gmail.com"
+                }
+            }
+            issueManagement {
+                system = "GitHub Issues"
+                url = "https://github.com/teaconmc/content-disposition/issues"
+            }
+            scm {
+                url = "https://github.com/teacon/content-disposition"
+                connection = "scm:git:git://github.com/teacon/content-disposition.git"
+                developerConnection = "scm:git:ssh://github.com/teacon/content-disposition.git"
+            }
+        }
+        artifact(tasks.jar)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
-rootProject.name = "content-disposition"
-
+rootProject.name = "ContentDisposition"
